a5467f83b30fd14eab7e4f808482b605d9b06dbc
Myeong-Seob <1996ms@naver.com>
2020-12-12 20:20:30 +0900

    Home Container

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+export default class extends React.Component {
+  state = {
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
+    error: null,
+    loading: true,
+  };
+  render() {
+    const { nowPlaying, popular, upcoming, error, loading } = this.state;
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        popular={popular}
+        upcoming={upcoming}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
