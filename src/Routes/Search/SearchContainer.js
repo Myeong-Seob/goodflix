a76a1e99814a8f43b8505bc92a59ede73e7151f6
Myeong-Seob <1996ms@naver.com>
2020-12-14 20:32:25 +0900

    Search Container API

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi, tvApi } from "API";
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
 
@@ -9,6 +10,32 @@ export default class extends React.Component {
     loading: false,
     error: null,
   };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      this.setState({ loading: true });
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      this.setState({ movieResults, tvResults });
+      
+    } catch {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, loading, error } = this.state;
     return (
@@ -18,6 +45,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
