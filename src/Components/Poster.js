6746799e569183f8976a3c45290a3333a89a9cd4
Myeong-Seob <1996ms@naver.com>
2020-12-16 22:25:35 +0900

    Poster Component Styling

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,17 +3,50 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+    font-size:12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+    background-image:url(${props=>`https://image.tmdb.org/t/p/w300${props.bgUrl}`});
+    height:180px;
+    background-size:cover;
+    border-radius:4px;
+    background-position:center center;
+    transition: opacity .2s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+position:absolute;
+    bottom: 5px;
+    right:5px;
+    opacity:0;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+    margin-bottom:5px;
+    &:hover{
+        ${Image}{
+            opacity:0.3;
+        }
+        ${Rating}{
+            opacity:1;
+        }
+    }
+    position:relative;
+`;
 
-const Title = styled.span``;
 
-const Year = styled.span``;
+
+const Title = styled.span`
+    display:block;
+    margin-bottom:3px;
+`;
+
+const Year = styled.span`
+    font-size:10px;
+    color:rgba(255,255,255,0.5);
+`;
 
 const Poster = ({ id,imageUrl, title, rating, year, isMove = false }) => (
   <Link to={isMove ? `/movie/${id}` : `/show/${id}`}>
@@ -27,7 +60,7 @@ const Poster = ({ id,imageUrl, title, rating, year, isMove = false }) => (
           {rating}/10
         </Rating>
       </ImageContainer>
-      <Title>{title}</Title>
+      <Title>{title.length >15 ? `${title.substring(0,18)}...` :title }</Title>
       <Year>{year}</Year>
     </Container>
   </Link>
