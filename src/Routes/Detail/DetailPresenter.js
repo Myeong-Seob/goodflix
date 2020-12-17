62836e09c0591bf45a499ccd6958867b62e4ddcc
Myeong-Seob <1996ms@naver.com>
2020-12-17 20:12:56 +0900

    Detail Presenter Styling

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -7,7 +7,6 @@ const Container = styled.div`
   height: calc(100vh - 50px);
   width: 100%;
   position: relative;
-  padding: 50px;
 `;
 
 const Backdrop = styled.div`
@@ -31,9 +30,36 @@ const Content = styled.div`
   position: relative;
 `;
 
+const Data = styled.div`
+  padding: 30px;
+  width: 60%;
+`;
+
+const Title = styled.h3`
+  font-size: 32px;
+  margin-bottom: 20px;
+`;
+
+const ItemContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+  margin: 0 10px;
+`;
+
+const Overview = styled.p`
+  font-size: 12px;
+  opacity: 0.7;
+  line-height: 1.5;
+  width: 50%;
+`;
+
 const Cover = styled.div`
   z-index: 1;
-  width: 30%;
+  width: 40%;
   height: 100%;
   background-image: url(${(props) => props.bgImage});
   background-position: center center;
@@ -57,6 +83,35 @@ const DetailPresenter = ({ result, error, loading }) =>
               : null
           }
         />
+        <Data>
+          <Title>
+            {result.original_title
+              ? result.original_title
+              : result.original_name}
+          </Title>
+          <ItemContainer>
+            <Item>
+              {result.release_date
+                ? result.release_date.substring(0, 4)
+                : result.first_air_date.substring(0, 4)}
+            </Item>
+            <Divider>·</Divider>
+            <Item>
+              ⏱ {result.runtime ? result.runtime : result.episode_run_time[0]}{" "}
+              min
+            </Item>
+            <Divider>·</Divider>
+            <Item>
+              {result.genres &&
+                result.genres.map((genre, index) =>
+                  index === result.genres.length - 1
+                    ? genre.name
+                    : `${genre.name} / `
+                )}
+            </Item>
+          </ItemContainer>
+          <Overview>{result.overview}</Overview>
+        </Data>
       </Content>
     </Container>
   );
